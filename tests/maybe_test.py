ac42a288245eb292911854f71fd1a3fcbcd9a09f
Francois Sarradin <fsarradin@kyriba.com>
2014-04-30 13:33:20 +0200

    add test on fold and __iter__

--- tests/maybe_test.py
+++ tests/maybe_test.py
@@ -22,6 +22,20 @@ class MaybeTest(TestCase):
     def test_Nothing_is_equal_to_Nothing(self):
         self.assertEqual(Nothing, Nothing)
 
+    def test_iter_on_Just(self):
+        result = 0
+        for value in Just(10):
+            result = value
+
+        self.assertEqual(10, result)
+
+    def test_iter_on_Nothing(self):
+        result = 0
+        for value in Nothing:
+            result = value
+
+        self.assertEqual(0, result)
+
 
 class MaybeDefinedTest(TestCase):
     def test_Just_is_defined(self):
@@ -51,3 +65,13 @@ class MaybeMapTest(TestCase):
     def test_map_flatmap_on_Nothing(self):
         result = Just(10).flatmap(lambda x: Nothing.map(lambda y: x + y))
         self.assertEqual(Nothing, result)
+
+
+class MaybeFoldTest(TestCase):
+    def test_fold_of_Just_gives_result(self):
+        result = Just(10).fold(5, lambda x, y: x + y)
+        self.assertEqual(15, result)
+
+    def test_fold_of_Nothing_shold_be_initial_value(self):
+        result = Nothing.fold(5, lambda x, y: x + y)
+        self.assertEqual(5, result)
